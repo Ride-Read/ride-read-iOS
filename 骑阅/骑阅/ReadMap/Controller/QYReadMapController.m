0e25639a1a973268323bfb2ddf465a709cf26c52
jsonsnow <1183010734@qq.com>
2017-04-18 14:49:13 +0800

    fix

--- 骑阅/骑阅/ReadMap/Controller/QYReadMapController.m
+++ 骑阅/骑阅/ReadMap/Controller/QYReadMapController.m
@@ -241,7 +241,7 @@
             self.showSendCycleView = NO;
         }];
         send.frame = CGRectMake(45, cl_caculation_3y(500), kScreenWidth-90, 45);
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
             [send show];
             
